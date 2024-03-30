a26ba5ba89d29588cbda44442663c41b610381b1
fi136pix <filipecocinel@gmail.com>
2024-03-30 01:00:34 -0300

    google map showing

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Map from "./components/map/Map.tsx";
+
+function App() {
+
+  return (
+    <>
+        <h1>
+            OLA
+        </h1>
+        <Map></Map>
+        </>
+)
+}
+
+export default App
