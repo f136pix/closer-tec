25b921182763a8bbac2149905f11bfcbb0ee7841
fi136pix <filipecocinel@gmail.com>
2024-04-29 23:12:17 -0300

    auth ongoing

--- web/src/lib/utils.tsx
+++ web/src/lib/utils.tsx
@@ -4,3 +4,7 @@ import {ClassNameValue, twMerge} from "tailwind-merge";
 export function cn(...inputs: ClassNameValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export async function saveCookie(key: string, value: string) {
+    document.cookie = `${key}=${value}; path=/`;
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,16 +1,17 @@
 import {Route, Routes} from "react-router-dom";
 
+import LoginPage from "../components/LoginPage/LoginPage.tsx";
 import DashboardPage from "../components/DashboardPage/components/DashboardPage/DashboardPage.tsx";
-import LoginPage from "../components/DashboardPage/components/LoginPage/LoginPage.tsx";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage/>}></Route>
-            <Route path="/app" element={<DashboardPage/>}/>
-        </Routes>
+            <Route path="/auth" element={<LoginPage/>}></Route>
+            <Route path="/" element={<DashboardPage/>}></Route>
+</Routes>
 
-    );
+)
+    ;
 };
 
 export default AppRoutes;
